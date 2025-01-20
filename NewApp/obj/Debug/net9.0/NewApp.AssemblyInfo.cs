87af0e5a928daa0c029c7b8d3973655778beffa5
Nguyen-TienDuc <nemo811107@gmail.com>
2025-01-20 22:32:05 +0700

    BTH_CSS

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34fc55bd5386bfce40cff7c6e1934781d8685177")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+215dc9903053ca82b9ac8395c1b0c060d712e581")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
