ffadc2b14634e145e836da5f842c1d25bf016e0f
Duc-2003-qq <nemo811107@gmail.com>
2025-03-12 11:39:57 +0700

    Initial commit

---
+++ MvcMovie/Migrations/20250312030031_Create_table_Employee.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class Create_table_Employee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Persons_PersonId",
+                table: "Employees");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonId",
+                table: "Employees",
+                newName: "FullName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Employees",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees",
+                column: "EmployeeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "Employees",
+                newName: "PersonId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Employees",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Employees",
+                table: "Employees",
+                column: "PersonId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Persons_PersonId",
+                table: "Employees",
+                column: "PersonId",
+                principalTable: "Persons",
+                principalColumn: "PersonId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
